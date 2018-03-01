858ccb86df353548bd120273ca68b72a0ae4f41b
SirMwazv <36504999+SirMwazv@users.noreply.github.com>
2018-03-01 15:47:47 +0200

    Add files via upload
    14 tests left

--- FSharpKoans/AboutRev.fs
+++ FSharpKoans/AboutRev.fs
@@ -10,7 +10,12 @@ module ``14: Reversing a list`` =
     [<Test>]
     let ``01 Reversing a list, the hard way`` () =
         let rev (xs : 'a list) : 'a list =
-            __ // write a function to reverse a list here.
+             let rec innerRev xs revList = 
+                match xs with
+                |[] -> revList
+                |a :: restA -> innerRev restA (a::revList)
+             innerRev xs []
+             // write a function to reverse a list here.
         rev [9;8;7] |> should equal [7;8;9]
         rev [] |> should equal []
         rev [0] |> should equal [0]
